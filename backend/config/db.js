5d2e30875b7bcde6d18ad58d3eeb91b0a10fc0bd
Odree123 <audreypendo@gmail.com>
2026-04-06 11:49:22 +0300

    database configuration file

---
+++ backend/config/db.js
@@ -0,0 +1,25 @@
+
+require('dotenv').config();
+const {Pool} =require('pg');
+
+const pool = new Pool({
+    user:process.env.DB_USER,
+    host:process.env.DB_HOST,
+    database:process.env.DB_NAME,
+    password:process.env.DB_PASSWORD,
+    port:process.env.DB_PORT
+});
+
+pool.connect((err) => {
+    if (err){
+        console.error('Connection Error',err.stack)
+    }
+    else{
+        console.log('Connected to Afya_connect database')
+    }
+   
+
+
+});
+
+module.exports = pool;
