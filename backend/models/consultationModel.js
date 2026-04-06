f17b2e3c2decb165405efdaafbbf65d0b3b2263d
Odree123 <audreypendo@gmail.com>
2026-04-06 11:13:44 +0300

    added getPatientConsultation to view patients consultations

--- backend/models/consultationModel.js
+++ backend/models/consultationModel.js
@@ -16,7 +16,7 @@ const getDoctorConsultations = async (doctor_id) => {
     const result = await pool.query(
         `SELECT consultations.*, users.name AS patient_name
          FROM consultations
-         JOIN users ON consultations.patient_id = users.id
+         LEFT JOIN users ON consultations.patient_id = users.id
          WHERE consultations.doctor_id = $1
          ORDER BY consultations.created_at DESC`,
         [doctor_id]
@@ -31,10 +31,10 @@ const acceptConsultation = async (consultation_id) => {
         `UPDATE consultations
          SET status = 'accepted'
          WHERE id = $1
-         RETURNING *`,
+         RETURNING consultations.*, 
+         (SELECT name FROM users WHERE id = consultations.patient_id) AS patient_name`,
         [consultation_id]
     );
-
     return result.rows[0];
 };
 
@@ -80,7 +80,23 @@ const completeConsultation = async (consultation_id) => {
     );
 
     return result.rows[0];
+    
 };
-module.exports = { createConsultationRequest, getDoctorConsultations, acceptConsultation, payConsultation, startConsultation, getConsultationById, completeConsultation };
+const getPatientConsultations = async (patient_id) => {
+    const result = await pool.query(
+        `SELECT consultations.*, users.name AS doctor_name
+         FROM consultations
+         LEFT JOIN users ON consultations.doctor_id = users.id
+         WHERE consultations.patient_id = $1
+         ORDER BY consultations.created_at DESC`,
+        [patient_id]
+    );
+     console.log("Result:", result.rows);
+    return result.rows;
+};
+
+
+
+module.exports = { createConsultationRequest, getDoctorConsultations, acceptConsultation, payConsultation, startConsultation, getConsultationById, completeConsultation ,getPatientConsultations};
 
   
