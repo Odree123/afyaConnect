a19fca3b030c0d7ef196b59d311bd1150cef8044
Odree123 <audreypendo@gmail.com>
2026-04-28 22:22:22 +0300

    removed loadConsultations

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -927,7 +927,7 @@ document.querySelector('.logout-link')?.addEventListener('click', (e) => {
 // ===========================
 loadNotificationCounts();
 loadDoctors();
-loadConsultations();
+
 
 // ===========================
 // PRINT PRESCRIPTION
